488677ca7ad47445ea711f9ba6da548919f9a8e7
Trollicorn <muddin3@stuy.edu>
2018-12-13 16:54:17 -0500

    your phone has been repaired

--- phone.c
+++ phone.c
@@ -13,31 +13,36 @@
 int main(int argc, char * argv[]){
 
 	if (argc != 2){
-		printf("the number you have dialed is not available\n
-			please only add only one of the following arguments:\n
-			'-c': make the thing\n
-			'-r': remove the thing\n
-			'-v': view the thing\n")
+		printf("the number you have dialed is not available\n"
+			"please only add only one of the following arguments:\n"
+			"'-c': make the thing\n"
+			"'-r': remove the thing\n"
+			"'-v': view the thing\n");
 		return -1;
 	}
 	
 	if (!strcmp(argv[0],"-c")){ //creation
 		semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0666);
 		shmget(KEY, 200, IPC_CREAT | IPC_EXCL | 0666);
-		open("story.txt", O_CREAT | O_EXCL);
+		int fd = open("story.txt", O_CREAT | O_EXCL);
+		close(fd);
 	}
 	if (!strcmp(argv[0],"-r")){ //destruction
-		semid = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0666); 
+		int semid = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0666); 
 		semctl(semid, 0, IPC_RMID);
-		shmid = shmget(KEY, 200, IPC_CREAT | IPC_EXCL | 0666);
+		int shmid = shmget(KEY, 200, IPC_CREAT | IPC_EXCL | 0666);
 		shmctl(shmid, IPC_RMID, NULL);
-		fd = open("story.txt", O_RDONLY |  O_CREAT | O_EXCL);
+		int fd = open("story.txt", O_RDONLY |  O_CREAT | O_EXCL);
 		char s[4000];
 		read(fd,s,sizeof(s));
 		printf("%s",s);
 		unlink("story.txt");		
 	}
-	if (!strcmp(argv[0], "-v"){
-		v
+	if (!strcmp(argv[0], "-v")){
+		int fd = open("story.txt", O_RDONLY |  O_CREAT | O_EXCL);
+		char s[4000];
+		read(fd,s,sizeof(s));
+		printf("%s",s);
+		close(fd);
 	}
 }
